143becfdfba3b0c9a32f6134f94c88575655c132
Aldrence <aldrencemcodiwuor@gmail.com>
2024-10-10 19:20:01 +0300

    first commit

--- answers.sql
+++ answers.sql
@@ -1,71 +1,65 @@
--- Creating a database
-CREATE DATABASE hospital_db;
-USE hospita_db;
---Creating Tables
---Table Name: patients
-CREATE TABLE patients(
-    patient_id INT PRIMARY KEY AUTO_INCREMENT,
-    first_name VARCHAR(255) NOT NULL,
-    last_name VARCHAR(255) NOT NULL,
-    date_of_birth DATE NOT NULL,
-    gender VARCHAR(255),
-    language VARCHAR(255) NOT NULL
-);
---Table Name: providers
-CREATE TABLE providers(
-provider_id INT PRIMARY KEY AUTO_INCREMENT,
-first_name VARCHAR(255) NOT NULL,
-last_name VARCHAR(255) NOT NULL,
-provider_speciality VARCHAR(255) NOT NULL,
-email_address VARCHAR(255),
-phone_number VARCHAR(255),
-date_joined DATE NOT NULL
-);
---Table Name: visits
-CREATE TABLE visits(
-visit_id INT PRIMARY KEY AUTO_INCREMENT,
-patient_id INT,
-FOREIGN KEY(patient_id) REFERENCES patients(patient_id),
-provider_id INT,
-FOREIGN KEY(provider_id) REFERENCES providers(provider_id),
-date_of_visit DATE NOT NULL,
-date_scheduled DATE NOT NULL,
-visit_department_id INT NOT NULL,
-visit_type VARCHAR(255) NOT NULL,
-blood_pressure_systollic INT,
-blood_pressure_diastollic DECIMAL,
-pulse DECIMAL,
-visit_status VARCHAR(255) NOT NULL
-);
---Table Name: ed_visits
-CREATE TABLE ed_visits(
-ed_visit_id INT PRIMARY KEY AUTO_INCREMENT,
-visit_id INT,
-FOREIGN KEY(visit_id) REFERENCES visits(visit_id),
-patient_id INT,
-FOREIGN KEY(patient_id) REFERENCES patients(patient_id),
-acuity INT NOT NULL,
-reason_for_visit VARCHAR(255) NOT NULL,
-disposition VARCHAR(255) NOT NULL
-);
---Table Name: admission
-CREATE TABLE admission(
-admission_id INT PRIMARY KEY AUTO_INCREMENT,
-patient_id INT,
-FOREIGN KEY(patient_id) REFERENCES patients(patient_id),
-admission_date DATE NOT NULL,
-discharge_date DATE NOT NULL,
-discharge_disposition VARCHAR(255) NOT NULL,
-service VARCHAR(255) NOT NULL,
-primary_diagnosis VARCHAR(255) NOT NULL
-);
---Table Name: discharges
-CREATE TABLE discharges(
-discharge_id INT PRIMARY KEY AUTO_INCREMENT,
-admission_id INT,
-FOREIGN KEY(admission_id) REFERENCES admission(admission_id),
-patient_id INT,
-FOREIGN KEY(patient_id) REFERENCES patients(patient_id),
-discharge_date DATE NOT NULL,
-discharge_disposition VARCHAR(255) NOT NULL
-);
+-- Basic Data Retrieval
+-- USE hospital;
+
+-- Question 1.1
+SELECT first_name, last_name, date_of_birth 
+FROM patients;
+
+-- Question 1.2
+SELECT provider_id, first_name, provider_specialty
+FROM providers;
+
+-- Question 2.1
+SELECT * FROM patients
+WHERE first_name LIKE'Ab%';
+
+-- Question 2.2
+SELECT * FROM providers
+WHERE provider_specialty LIKE '%y';
+
+-- Question 3.1
+SELECT * FROM patients
+WHERE date_of_birth > 1980-01-01;
+
+-- Question 3.2
+SELECT * FROM ed_visits 
+WHERE acuity >= 2;
+
+-- Question 4.1
+SELECT * FROM patients
+WHERE language = 'Spanish';
+
+-- Question 4.2
+SELECT * FROM ed_visits 
+WHERE reason_for_visit = 'Migraine' 
+AND ed_disposition = 'Admitted';
+
+-- Question 4.3
+SELECT *FROM patients
+WHERE date_of_birth BETWEEN '1975-01-01' AND '1980-12-31';
+
+-- Question 5.1
+SELECT first_name, last_name 
+FROM patients 
+ORDER BY last_name ASC;
+
+-- Question 5.2
+SELECT * FROM visits 
+ORDER BY date_of_visit DESC;
+
+-- Question 6.1
+SELECT * FROM admissions
+JOIN discharges ON admissions.admission_id = discharges.admission_id
+WHERE admissions.primary_diagnosis = 'Stroke'
+AND discharges.discharge_disposition = 'Home';
+
+-- Question 6.2
+SELECT * FROM providers
+WHERE date_joined > '1995-12-31' 
+AND provider_specialty = 'Pediatrics' 
+OR provider_specialty = 'Cardiology';
+
+-- Bonus Challange
+SELECT * FROM discharges
+WHERE discharge_disposition = 'Home'
+AND discharge_date BETWEEN '2018-03-01' AND '2018-03-07';
